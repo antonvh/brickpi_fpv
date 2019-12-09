5afef475eaaf6ba73aa0e5ab75e2dffbfd901863
Anton Vanhoucke <antonvh@gmail.com>
2019-12-09 09:50:33 +0100

    cleaned up settings

--- settings.py
+++ settings.py
@@ -1,16 +1,5 @@
-BLUETOOTH = False
+# Video config
 VIDEO = True
-
-RECV_BUFFER = 2048  # Advisable to keep it as an exponent of 2
-DATA_PORT = 50007  # data port
-VIDEO_PORT = 5000
-
-BRICK_ADDR = '00:16:53:0E:1C:AC'  # change this to the bt address of your brick
-BRICK_NAME = 'NXT'  # fallback if the address is not found
-
-MOTOR_CMD_RATE = 20  # Max number of motor commands per second
-BT_CMD_RATE = 30
-
 RASPIVID_CMD = "raspivid -t 999999 -b 2000000 -rot 90 -o -"
 STREAM_CMD = "gst-launch-1.0 -e -vvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host={0} port={1}"
 
@@ -19,4 +8,19 @@ VIDEO_W = 1280
 VIDEO_H = 720
 BITRATE = 10000000
 
+# Data connections
+PC_HOST = "192.168.188.22"
+EV3_HOST = "ev3dev"  # An IP address will work too
+RECV_BUFFER = 2048  # Advisable to keep it as an exponent of 2
+DATA_PORT = 50007  # data port
+VIDEO_PORT = 5000
+
+# Bluetooth NXT slave config
+BLUETOOTH = False
+BRICK_ADDR = '00:16:53:0E:1C:AC'  # change this to the bt address of your brick
+BRICK_NAME = 'NXT'  # fallback if the address is not found
+BT_CMD_RATE = 30
+
+# Motor configs
+MOTOR_CMD_RATE = 20  # Max number of motor commands per second
 STICK_RANGE = (-32768, 32768)
